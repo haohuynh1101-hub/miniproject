703b7df29081eb65ad4c7382973d5dc55119a098
hhh <haohuynh1101-hub>
2021-06-06 20:50:37 +0700

    fix config file

--- next.config.js
+++ next.config.js
@@ -1,73 +1,32 @@
-const withPlugins = require('next-compose-plugins');
-const withLess = require('@zeit/next-less');
-const lessToJS = require('less-vars-to-js');
-const withImages = require('next-images');
-const withSass = require(`@zeit/next-sass`);
-const { i18n } = require('./next-i18next.config')
+/* eslint-disable @typescript-eslint/no-var-requires */
+const withCSS = require("@zeit/next-css");
+const withLess = require("@zeit/next-less");
+const withSass = require("@zeit/next-sass");
+const withPlugins = require("next-compose-plugins");
 
-const fs = require('fs');
-const path = require('path');
-
-// Where your antd-custom.less file lives
-const themeVariables = lessToJS(
-	fs.readFileSync(path.resolve(__dirname, './styles/antd.less'), 'utf8')
-);
-
+const plugins = [withCSS, withLess, withSass];
 const nextConfig = {
-	images: {
-		domains: [],
-	},
-	i18n
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ["@svgr/webpack"],
+    });
+
+    return config;
+  },
+
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/",
+        permanent: true,
+      },
+    ];
+  },
+  lessLoaderOptions: {
+    javascriptEnabled: true,
+  },
 };
 
-module.exports = withPlugins(
-	[
-		[
-			withSass({
-				cssModules: true,
-				...withLess({
-					lessLoaderOptions: {
-						javascriptEnabled: true,
-						modifyVars: themeVariables, // make your antd custom effective
-						importLoaders: 0,
-					},
-					cssLoaderOptions: {
-						importLoaders: 3,
-						localIdentName: '[local]___[hash:base64:5]',
-					},
-					webpack: (config, { isServer }) => {
-						//Make Ant styles work with less
-						if (isServer) {
-							const antStyles = /antd\/.*?\/style.*?/;
-							const origExternals = [...config.externals];
-							config.externals = [
-								(context, request, callback) => {
-									if (request.match(antStyles)) return callback();
-									if (typeof origExternals[0] === 'function') {
-										origExternals[0](context, request, callback);
-									} else {
-										callback();
-									}
-								},
-								...(typeof origExternals[0] === 'function'
-									? []
-									: origExternals),
-							];
-
-							config.module.rules.unshift({
-								test: antStyles,
-								use: 'null-loader',
-							});
-
-							config.resolve.alias['@'] = path.resolve(__dirname);
-							return config;
-						}
-						return config;
-					},
-				}),
-			}),
-		],
-		[withImages],
-	],
-	nextConfig
-);
+module.exports = withPlugins(plugins, nextConfig);
