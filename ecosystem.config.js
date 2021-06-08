bbad4d49b0f654fcca27da53bb6c923eba33094c
Hao Huynh <haohuynh1101-hub>
2021-06-08 15:46:08 +0700

    ecosystem

---
+++ ecosystem.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  apps: [
+    {
+      name: 'dashboard-template',
+      script: 'serve',
+      env: {
+        PM2_SERVE_PATH: './.next/',
+        PM2_SERVE_PORT: 4040,
+      },
+    },
+  ],
+
+  deploy: {
+    production: {
+      user: 'SSH_USERNAME',
+      host: 'SSH_HOSTMACHINE',
+      ref: 'origin/master',
+      repo: 'GIT_REPOSITORY',
+      path: 'DESTINATION_PATH',
+      'pre-deploy-local': '',
+      'post-deploy':
+        'npm install && pm2 reload ecosystem.config.js --env production',
+      'pre-setup': '',
+    },
+  },
+};
